987627d786697bd03179657fc812358ae177ad24
maciej.kuczynski <maciej.kuczynski@pl.ibm.com>
2019-07-16 16:06:12 +0200

    init commit

---
+++ statystykaopisowacz2.R
@@ -0,0 +1,15 @@
+#miary zmiennosci
+
+v1 <- c(1,2,3,4,5,6,6,7,8,9,10,11)
+v2 <- c(4,5,5,5,6,6,6,6,7,7,8,8)
+
+
+install.packages('devtools')
+library(devtools)
+
+#instalowanie paczki Bartka
+install_github('karabanb/BKmisc')
+library(BKmisc)
+BKmisc::start_proj()
+
+
